53c343f0722a031106f88e2d8fcfad35834d724f
sumanthakur93 <suman931980@gmail.com>
2024-06-13 17:51:24 +0530

    added database module

---
+++ app/db/database.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:1234@localhost/NewDB")
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+# Dependency to get DB session
+def get_db():
+    db = SessionLocal()
+    try:
+        print("fetching db")
+        yield db
+
+    finally:
+        db.close()
